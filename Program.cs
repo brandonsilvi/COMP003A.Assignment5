3494ef02d6a39e629a679c74467d79e5eb594379
Brandon Silvibarr <brandon.silvi@gmail.com>
2026-02-11 08:30:28 -0800

    repeat while loop along with starting methods section with the introduction method.

--- Program.cs
+++ Program.cs
@@ -6,6 +6,23 @@ namespace COMP003A.Assignment5
     {
         static void Main(string[] args)
         {
+            string runAgain = "y";
+            
+            //loop to repeat program execution
+            while (runAgain.ToLower() == "y")
+            {
+                
+            }
         }
+        
+        //methods section-----------------------------------
+        //methods exists to improve organization. These can be used as tools to call in the main
+        //to produce a specific action when called without having to retype the instructions for the action
+        static void DisplayIntroduction()  //method to display introduction
+        {
+            Console.WriteLine("Welcome to the Program Organizer!\n");
+        }
+        
+      
     }
 }
